8376ba91250ca0c2a25610f12dea13127052a800
FikryAS <azizfikry15@gmail.com>
2020-11-05 16:13:51 +0700

    home main

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_card_v2_recom_inisiated/main_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return MaterialApp(
+      home: MainPage(),
+    );
   }
 }
